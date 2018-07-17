dace51b30fde098ce5c7e327d0a7d6dcd6e48df7
Daksha Patel <dakshapatel100@gmail.com>
2018-07-17 18:01:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @all_five_words = (params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5].)
+    @all_five_words = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]
     "#{@all_five_words}"
   end
 
