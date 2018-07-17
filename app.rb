10a21a4adab635a719c1d6c1fdce16ad4000a89e
Daksha Patel <dakshapatel100@gmail.com>
2018-07-17 18:04:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,12 +17,13 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @phrase_number_of_times = params[:number].to_i.times params[:phrase]
-    
+
     "#{@phrase_number_of_times}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @all_five_words = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]
+    @all_five_words = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5
+      @all_five_words.gsub()/[\s,]/ ,"")
     binding.pry
     "#{@all_five_words}"
   end
