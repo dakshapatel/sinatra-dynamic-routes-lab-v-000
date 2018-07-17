5b6fd24c3af26c119079038af76bfebc691309d9
Daksha Patel <dakshapatel100@gmail.com>
2018-07-17 18:14:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,8 +24,8 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @all_five_words = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]
       @words_combined = @all_five_words.join(" ")
-
     "#{@words_combined}"
+    binding.pry
   end
 
 
