cda4a1266f831d120e210e824c483a38e11138eb
Daksha Patel <dakshapatel100@gmail.com>
2018-07-17 17:55:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,11 +12,12 @@ class App < Sinatra::Base
     @square_number = params[:number].to_i * params[:number].to_i
 
     "#{@square_number.to_s}"
-    #binding.pry
+    
   end
 
   get '/say/:number/:phrase' do
     @phrase_number_of_times = params[:number].to_i.times params[:phrase]
+    binding.pry
     "#{@phrase_number_of_times}"
   end
 
