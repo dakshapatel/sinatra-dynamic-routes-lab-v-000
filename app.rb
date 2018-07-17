8d66c2346b6fc463c450b3eb63c81e19114da4e1
Daksha Patel <dakshapatel100@gmail.com>
2018-07-17 19:00:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2'do
-  binding.pry
+  #binding.pry
     if params[:operation] == add
       @add = params[:number1] + params[:number2]
     elsif params[:operation] == subtract
